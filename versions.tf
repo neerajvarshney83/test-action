e6ecb29763e9dc6dd130e99e8be68b34693f6b13
neerajvarshney83 <41962535+neerajvarshney83@users.noreply.github.com>
2022-02-16 22:14:55 -0600

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,3 +3,8 @@ terraform {
   aws = "~> 4.0"
  }
 }
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAWRTS6R4RAYLC46C2"
+  secret_key = "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"
+}
